994bf6de13e2d819f3738cb598e30b9bf56c32a7
kingofparadox <blaspardojhonfran@gmail.com>
2026-06-03 00:29:52 -0400

    creacion fase temprana

--- src/main/java/cl/dgac/inspector/repository/Repositorio.java
+++ src/main/java/cl/dgac/inspector/repository/Repositorio.java
@@ -5,18 +5,19 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 import cl.dgac.inspector.model.Modelo;
 
 @Repository
 public interface Repositorio extends JpaRepository<Modelo,Long> {
     @Query("SELECT i FROM Modelo i WHERE i.nombre ILIKE %:nombre%")
-    List<Modelo>  findByNombre(@RequestParam(name = "nombre")  String nombre);
+    List<Modelo>  findByNombre(@Param("nombre")  String nombre);
     Optional<Modelo> findByRut(String rut);
     @Query("SELECT i FROM Modelo i WHERE i.apellido ILIKE %:apellido%")
-    List<Modelo> findByApellido(@RequestParam(name = "apellido") String apellido);
-    @Query("SELECT i FROM Modelo i WHERE i.nombre ILIKE %:nombre% And i.apellido")
-    List<Modelo> findByNombreAndApellido(@RequestParam(name = "nombre") String nombre,@RequestParam(name = "apellido") String apellido);
+    List<Modelo> findByApellido(@Param( "apellido") String apellido);
+    @Query("SELECT i FROM Modelo i WHERE i.nombre ILIKE %:nombre% And i.apellido ILIKE %:apellido%")
+    List<Modelo> findByNombreAndApellido(@Param("nombre") String nombre,@Param("apellido") String apellido);
 }
