f541527d37dec5199739b1324ba1e565cef519f3
kingofparadox <blaspardojhonfran@gmail.com>
2026-06-18 23:27:55 -0400

    clase exepciones actualizados

--- src/main/java/cl/dgac/inspector/dtos/DtoExepcion.java
+++ src/main/java/cl/dgac/inspector/dtos/DtoExepcion.java
@@ -1,12 +1,27 @@
 package cl.dgac.inspector.dtos;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-public record DtoExepcion(
-    LocalDateTime fecha,
-    int codigoHttp,
-    String error,
-    String mensaje,
-    String ruta
-){
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class DtoExepcion {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String mensaje;
+    private Map<String, String> detalles;
+    private String ruta;
+    public DtoExepcion(LocalDateTime timestamp, int status, String error,String mensaje, Map<String, String> detalles, String ruta) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.error = error;
+        this.mensaje = mensaje;
+        this.detalles = detalles;
+        this.ruta = ruta;
+    }
+
 }
--- src/main/java/cl/dgac/inspector/exepciones/ExepcionesGlobales.java
+++ src/main/java/cl/dgac/inspector/exepciones/ExepcionesGlobales.java
@@ -17,47 +17,47 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class ExepcionesGlobales {
+    private ResponseEntity<DtoExepcion> buildResponse(
+        HttpStatus status,
+        String mnesaje,
+        String ruta,
+        Map<String, String> detalles){
+        DtoExepcion dto = new DtoExepcion(
+            LocalDateTime.now(),
+            status.value(),
+            status.getReasonPhrase(),
+            mnesaje,
+            detalles,
+            ruta
+        );
+        return  ResponseEntity.status(status).body(dto);
+    }
     @ExceptionHandler(ErrorEnRecursos.class)
     public ResponseEntity<DtoExepcion> ErrorEnRecursos(ErrorEnRecursos ex,HttpServletRequest request){
-        DtoExepcion error = new DtoExepcion(
-            LocalDateTime.now(),
-            HttpStatus.NOT_FOUND.value(),
-            "recurso no encontrado",
-            ex.getMessage(),
-            request.getRequestURI()
-        );
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);}
-
+    return buildResponse(
+        HttpStatus.NOT_FOUND,ex.getMessage(),request.getRequestURI(),null);
+    }
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String,String>> exepcionesValicadionDto(MethodArgumentNotValidException ex){
+    public ResponseEntity<DtoExepcion> exepcionesValicadionDto(MethodArgumentNotValidException ex,HttpServletRequest request){
         Map<String,String> errores = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error)->{
             String campo=((FieldError)error).getField();
             String mensaje = error.getDefaultMessage();
             errores.put(campo,mensaje);
         });
-        return new ResponseEntity<>(errores,HttpStatus.BAD_REQUEST);}
+        return buildResponse(HttpStatus.BAD_REQUEST, 
+            "elementos en el json tienen problemas ",
+            request.getRequestURI(),
+            errores);
+        }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<DtoExepcion> interlServErerror(Exception ex, HttpServletRequest request){
-        DtoExepcion error = new DtoExepcion(
-            LocalDateTime.now(),
-            HttpStatus.INTERNAL_SERVER_ERROR.value(),
-            "error interno en el servidor",
-            "ocurrio un   error inesperado" +ex.getMessage(),
-            request.getRequestURI()
-        );
-        return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);}
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR,"ocurrio un   error inesperado " + ex.getMessage(),request.getRequestURI(), null);
+        }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<DtoExepcion> manejarDuplicados(DataIntegrityViolationException ex, HttpServletRequest request){
-        DtoExepcion error = new DtoExepcion(
-            LocalDateTime.now(),
-            HttpStatus.CONFLICT.value(),
-            "Error duplicado",
-            "El rut ingresado a se encuentra registrado ",
-            request.getRequestURI()
-        );
-        return new ResponseEntity<>(error,HttpStatus.CONFLICT);
+        return buildResponse(HttpStatus.CONFLICT,"Error duplicado",request.getRequestURI(),null);
     }
 }
